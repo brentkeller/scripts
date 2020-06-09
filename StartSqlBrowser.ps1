e2570d7d91996be485f82e22b9f4a63550cc5e7b
Brent Keller <brentkeller@gmail.com>
2020-06-09 12:40:23 -0400

    Add SQL Server Browser scripts

---
+++ StartSqlBrowser.ps1
@@ -0,0 +1,12 @@
+param([switch] $NoWait)
+$srvName = "SQL Server Browser"
+"Starting " + $srvName
+Start-Service $srvName
+# PowerShell cmdlet to check a service's status
+$serviceAfter = Get-Service $srvName
+$srvName + " is now " + $serviceAfter.status	
+    
+if (!$NoWait) {
+    Write-Host "Press any key to continue ..."
+    $_unused = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
