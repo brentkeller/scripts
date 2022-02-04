f5fe64ced4a43afa3f7359ecd67a4b2972649226
Brent Keller <brentkeller@gmail.com>
2022-02-04 10:09:50 -0500

    Add StartAzurite.ps1

---
+++ StartAzurite.ps1
@@ -0,0 +1,2 @@
+$host.UI.RawUI.WindowTitle = "azurite running"
+. "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\Extensions\Microsoft\Azure Storage Emulator\azurite.exe" -l C:\data\azemu
