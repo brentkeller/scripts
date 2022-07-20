0a5c085b2c94105292db7cdd9c7e3b541461dc76
Brent Keller <brentkeller@gmail.com>
2022-07-20 19:44:04 -0400

    azemu: Use NPM version of azurite

--- StartAzurite.ps1
+++ StartAzurite.ps1
@@ -1,2 +1,4 @@
 $host.UI.RawUI.WindowTitle = "azurite running"
-. "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\Extensions\Microsoft\Azure Storage Emulator\azurite.exe" -l C:\data\azemu
+# . "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\Extensions\Microsoft\Azure Storage Emulator\azurite.exe" -l C:\data\azemu
+# Run the NPM version:
+azurite -l C:\data\azemu
