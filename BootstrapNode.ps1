6fb1d1a323b99682362c666f5ce3a072b22c0f01
Brent Keller <brentkeller@gmail.com>
2024-08-19 10:21:02 -0400

    BootstrapNode: Remove linking bkcli
    This is now a dotnet CLI app

--- BootstrapNode.ps1
+++ BootstrapNode.ps1
@@ -12,10 +12,6 @@ npm i -g azurite
 
 # Link CLI apps
 
-Write-Host "Linking bkcli"
-Set-Location C:\dev\nodejs\bkcli
-npm link
-
 Write-Host "Linking devresults-cli"
 Set-Location C:\dev\dr\devresults-cli
 npm link
